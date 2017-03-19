2d5468a256024ea7af139ae9b16180abb002d46a
hxk <15088133707@163.com>
2017-03-19 21:26:27 +0800

    fix cannot get the pagenum content
    get the pagename content

--- MagnetURL/app/src/main/java/com/superurl/magneturl/common/MagNetSearch.java
+++ MagnetURL/app/src/main/java/com/superurl/magneturl/common/MagNetSearch.java
@@ -1,12 +1,11 @@
 package com.superurl.magneturl.common;
 
 
-import android.util.Log;
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
@@ -15,9 +14,8 @@ import java.util.List;
 
 public class MagNetSearch implements ISearch {
     private final String mHeadUrl = "http://www.btany.com/search/";
+    private final int pagenum = 20;
     private List<MagnetUrl> mList = null;
-    private final int pagenum = 10;
-
 
     @Override
     public List<MagnetUrl> getSearch(String content) {
@@ -25,8 +23,10 @@ public class MagNetSearch implements ISearch {
         mList = new ArrayList<MagnetUrl>();
         for (int i = 1; i < this.pagenum; i++) {
             list = getSearch(content, i);
-            if (!list.isEmpty() && list.size() != 0) {
-                mList.add(list.get(i));
+            if(list != null) {
+                for (int j = 0; j < list.size(); j++) {
+                    mList.add(list.get(j));
+                }
             }
         }
         return mList;
@@ -36,7 +36,7 @@ public class MagNetSearch implements ISearch {
         String s = content + "-first-asc-" + pagenum;
         String pageurl = null;
         List<MagnetUrl> mMagnetUrls = new ArrayList<MagnetUrl>();
-        MagnetUrl magneturl = new MagnetUrl();
+        MagnetUrl magneturl;
         try {
             pageurl = mHeadUrl + URLEncoder.encode(s, "UTF-8");
             Document doc = null;
@@ -45,14 +45,16 @@ public class MagNetSearch implements ISearch {
                         .userAgent("Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:47.0) Gecko/20100101 Firefox/47.0")
                         .timeout(10000)
                         .get();
+                String title = doc.title();
             } catch (IOException e) {
                 e.printStackTrace();
             }
             Elements urls = doc.select("div.search-item");
-            if (urls.isEmpty() == true) {
+            if (urls.isEmpty()) {
                 //null
             }
             for (Element url : urls) {
+                magneturl = new MagnetUrl();
                 String itemtitle = url.select("div.item-title").select("a").text();
                 magneturl.setTitle(itemtitle);
                 String itemmagnet = url.select("div.item-bar").select("a.download[href^=magnet]").attr("href");
